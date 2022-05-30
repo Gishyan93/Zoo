899b7b24ddb1b5e2306ff48157b78de78b307bb2
Tigran Gishyan <tigran.gishian@gmail.com>
2022-05-31 01:05:26 +0400

    Added custom animal cell

--- Zoo/Scenes/Animals/AnimalsViewController.swift
+++ Zoo/Scenes/Animals/AnimalsViewController.swift
@@ -7,11 +7,50 @@
 
 import UIKit
 
+struct Animal {
+    var imageString: String
+    var name: String
+    var info: String
+}
+
+struct Animals {
+    static func getAnimals() -> [Animal] {
+        var animals: [Animal] = []
+        animals.append(
+            Animal(
+                imageString: "Mammals",
+                name: "Colibri",
+                info: "Lives in South America."
+            )
+        )
+        
+        animals.append(
+            Animal(
+                imageString: "Mammals",
+                name: "Colibri",
+                info: "Lives in South America."
+            )
+        )
+        
+        animals.append(
+            Animal(
+                imageString: "Mammals",
+                name: "Colibri",
+                info: "Lives in South America."
+            )
+        )
+        
+        return animals
+    }
+}
+
 class AnimalsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "DefaultCell")
+        tableView.register(
+            AnimalCell.self, forCellReuseIdentifier: "cell"
+        )
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -19,18 +58,21 @@ class AnimalsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return Animals.getAnimals().count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultCell", for: indexPath)
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
 
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
-
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "cell",
+            for: indexPath
+        ) as! AnimalCell
+        
+        cell.data = Animals.getAnimals()[indexPath.row]
+        
         return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
     }
 }
